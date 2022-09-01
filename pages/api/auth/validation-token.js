ddc36a59cf424feb12b44f481e2b40ea31f9edf6
HichamOujari <hichamoujari99@gmail.com>
2022-09-01 21:53:23 +0100

    slkjdas

--- pages/api/auth/validation-token.js
+++ pages/api/auth/validation-token.js
@@ -1,7 +1,15 @@
 let connection = require("../../../config/db");
 var md5 = require('md5');
+import NextCors from 'nextjs-cors';
 
 export default async function handler(req, res) {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
+
     let { token } = req.body;
 
     await connection.query("SELECT * FROM user WHERE token = ?", [token], async (err, result) => {
